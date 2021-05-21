11862d44b906f38358d2cc2c2d85abc7f8273b16
Obungaa <shingletonsmith@gmail.com>
2021-05-20 21:43:07 -0400

    fixed indentation

--- src/Cell.cpp
+++ src/Cell.cpp
@@ -13,63 +13,63 @@ void Cell::InitGrid() {
 	cellVertices.setPrimitiveType(sf::Quads);
 	cellVertices.resize(width * height * 4);
 
-    grid = new int *[width];
-    updateGrid = new bool *[width];
-    for(int i = 0; i < width; i++) {
-        grid[i] = new int [height];
-        updateGrid[i] = new bool [height];
-    }
+	grid = new int *[width];
+	updateGrid = new bool *[width];
+	for(int i = 0; i < width; i++) {
+		grid[i] = new int [height];
+		updateGrid[i] = new bool [height];
+	}
 	
-    for (int x = 0; x < width; ++x) {
-        for (int y = 0; y < height; ++y)
-        {
+	for (int x = 0; x < width; ++x) {
+		for (int y = 0; y < height; ++y)
+		{
 			grid[x][y] = 0;
 			updateGrid[x][y] = false;
 			
-            sf::Vertex* quad = &cellVertices[(x + y * width) * 4];
+			sf::Vertex* quad = &cellVertices[(x + y * width) * 4];
 
-            quad[0].position = sf::Vector2f(x * cellSize, y * cellSize);
-            quad[1].position = sf::Vector2f((x + 1) * cellSize, y * cellSize);
-            quad[2].position = sf::Vector2f((x + 1) * cellSize, (y + 1) * cellSize);
-            quad[3].position = sf::Vector2f(x * cellSize, (y + 1) * cellSize);
-        }
-    }
+			quad[0].position = sf::Vector2f(x * cellSize, y * cellSize);
+			quad[1].position = sf::Vector2f((x + 1) * cellSize, y * cellSize);
+			quad[2].position = sf::Vector2f((x + 1) * cellSize, (y + 1) * cellSize);
+			quad[3].position = sf::Vector2f(x * cellSize, (y + 1) * cellSize);
+		}
+	}
 }
 
 void Cell::ColorGrid() {
-    for (int x = 0; x < width; ++x) {
-        for (int y = 0; y < height; ++y)
-        {
-            sf::Vertex* quad = &cellVertices[(x + y * width) * 4];
+	for (int x = 0; x < width; ++x) {
+		for (int y = 0; y < height; ++y)
+		{
+			sf::Vertex* quad = &cellVertices[(x + y * width) * 4];
 			
-            quad[0].color = cellColors[grid[x][y]];
-            quad[1].color = cellColors[grid[x][y]];
-            quad[2].color = cellColors[grid[x][y]];
-            quad[3].color = cellColors[grid[x][y]];
-        }
-    }
+			quad[0].color = cellColors[grid[x][y]];
+			quad[1].color = cellColors[grid[x][y]];
+			quad[2].color = cellColors[grid[x][y]];
+			quad[3].color = cellColors[grid[x][y]];
+		}
+	}
 }
 
 void Cell::ResetUpdateGrid() {
-    for (int x = 0; x < width; ++x) {
-        for (int y = 0; y < height; ++y) {
-            updateGrid[x][y] = false;
-        }
-    }
+	for (int x = 0; x < width; ++x) {
+		for (int y = 0; y < height; ++y) {
+			updateGrid[x][y] = false;
+		}
+	}
 }
 
 void Cell::ChangeGrid(int x, int y, int state) {
-    grid[x][y] = state;
+	grid[x][y] = state;
 }
 void Cell::ChangeUpdateGrid(int x, int y, bool state) {
-    updateGrid[x][y] = state;
+	updateGrid[x][y] = state;
 }
 
 bool Cell::ReturnUpdateGrid(int x, int y) {
-    return updateGrid[x][y];
+	return updateGrid[x][y];
 }
 int Cell::ReturnGrid(int x, int y) {
-    return grid[x][y];
+	return grid[x][y];
 }
 
 void Cell::AddGravity(int x, int y, int cellNumber, int gravity) {
@@ -79,11 +79,11 @@ void Cell::AddGravity(int x, int y, int cellNumber, int gravity) {
 }
 
 void Cell::SwapCells(int xOne, int yOne, int xTwo, int yTwo) {
-    int one = grid[xOne][yOne];
-    int two = grid[xTwo][yTwo];
+	int one = grid[xOne][yOne];
+	int two = grid[xTwo][yTwo];
 
-    grid[xOne][yOne] = two;
-    grid[xTwo][yTwo] = one;
+	grid[xOne][yOne] = two;
+	grid[xTwo][yTwo] = one;
 }
 
 void Cell::UpdateSand(int x, int y) {
@@ -105,18 +105,18 @@ void Cell::UpdateSand(int x, int y) {
 		else if (ReturnGrid(x - 1, y + 1) == 0) {
 			ChangeGrid(x, y, 0);
 
-            if (ReturnGrid(x + 1, y + 1) == 1 || ReturnGrid(x + 1, y + 1) == 3 || ReturnGrid(x + 1, y + 1) == 4) {
-                ChangeGrid(x - 1, y + 1, 1);
-                ChangeUpdateGrid(x - 1, y + 1, true);
-            }
+			if (ReturnGrid(x + 1, y + 1) == 1 || ReturnGrid(x + 1, y + 1) == 3 || ReturnGrid(x + 1, y + 1) == 4) {
+				ChangeGrid(x - 1, y + 1, 1);
+				ChangeUpdateGrid(x - 1, y + 1, true);
+			}
 		}
 		else if (ReturnGrid(x + 1, y + 1) == 0) {
-            ChangeGrid(x, y, 0);
+			ChangeGrid(x, y, 0);
 
-            if (ReturnGrid(x - 1, y + 1) == 1 || ReturnGrid(x - 1, y + 1) == 3 || ReturnGrid(x - 1, y + 1) == 4) {
-                ChangeGrid(x + 1, y + 1, 1);
-                ChangeUpdateGrid(x + 1, y + 1, true);
-            } 
+			if (ReturnGrid(x - 1, y + 1) == 1 || ReturnGrid(x - 1, y + 1) == 3 || ReturnGrid(x - 1, y + 1) == 4) {
+				ChangeGrid(x + 1, y + 1, 1);
+				ChangeUpdateGrid(x + 1, y + 1, true);
+			} 
 		}
 		else {
 			ChangeGrid(x, y, 1);
@@ -129,91 +129,91 @@ void Cell::UpdateSand(int x, int y) {
 }
 
 void Cell::UpdateWater(int x, int y) {
-    if (y == height - 1 || x == width - 1 || x == 0) {
-        ChangeGrid(x, y, 2);
-    } 
+	if (y == height - 1 || x == width - 1 || x == 0) {
+		ChangeGrid(x, y, 2);
+	} 
 	else if (ReturnGrid(x, y + 1) == 1 || ReturnGrid(x, y + 1) == 2 || ReturnGrid(x, y + 1) == 3 || ReturnGrid(x, y + 1) == 4) {
-        ChangeGrid(x, y, 0);
+		ChangeGrid(x, y, 0);
 
-        if (ReturnGrid(x - 1, y) == 0 && ReturnGrid(x + 1, y) == 0){
-            ChangeGrid(x, y, 0);
-    
-            if (y % 2 == 0) {
-                ChangeGrid(x - 1, y, 2);
-                ChangeUpdateGrid(x - 1, y, true);
-            } else {
-                ChangeGrid(x + 1, y, 2);
-                ChangeUpdateGrid(x + 1, y, true);
-            }
-        }
+		if (ReturnGrid(x - 1, y) == 0 && ReturnGrid(x + 1, y) == 0){
+			ChangeGrid(x, y, 0);
+	
+			if (y % 2 == 0) {
+				ChangeGrid(x - 1, y, 2);
+				ChangeUpdateGrid(x - 1, y, true);
+			} else {
+				ChangeGrid(x + 1, y, 2);
+				ChangeUpdateGrid(x + 1, y, true);
+			}
+		}
 
-        else if (ReturnGrid(x - 1, y) == 0) {
-            ChangeGrid(x, y, 0);
+		else if (ReturnGrid(x - 1, y) == 0) {
+			ChangeGrid(x, y, 0);
 
-            if (ReturnGrid(x + 1, y) == 1 || ReturnGrid(x + 1, y) == 2 || ReturnGrid(x + 1, y) == 3 || ReturnGrid(x + 1, y) == 4) {
-                ChangeGrid(x - 1, y, 2);
-                ChangeUpdateGrid(x - 1, y, true);
-            }
-        }
+			if (ReturnGrid(x + 1, y) == 1 || ReturnGrid(x + 1, y) == 2 || ReturnGrid(x + 1, y) == 3 || ReturnGrid(x + 1, y) == 4) {
+				ChangeGrid(x - 1, y, 2);
+				ChangeUpdateGrid(x - 1, y, true);
+			}
+		}
 
-        else if (ReturnGrid(x + 1, y) == 0) {
-            ChangeGrid(x, y, 0);
+		else if (ReturnGrid(x + 1, y) == 0) {
+			ChangeGrid(x, y, 0);
 
-            if (ReturnGrid(x - 1, y) == 1 || ReturnGrid(x - 1, y) == 2 || ReturnGrid(x - 1, y) == 3 || ReturnGrid(x - 1, y) == 4) {
-                ChangeGrid(x + 1, y, 2);
-                ChangeUpdateGrid(x + 1, y, true);
-            }
-        }
+			if (ReturnGrid(x - 1, y) == 1 || ReturnGrid(x - 1, y) == 2 || ReturnGrid(x - 1, y) == 3 || ReturnGrid(x - 1, y) == 4) {
+				ChangeGrid(x + 1, y, 2);
+				ChangeUpdateGrid(x + 1, y, true);
+			}
+		}
 
-        else if (ReturnGrid(x - 1, y + 1) == 0 && ReturnGrid(x + 1, y + 1) == 0){
-            ChangeGrid(x, y, 0);
-    
-            if (y % 2 == 0) {
-                ChangeGrid(x - 1, y + 1, 2);
-                ChangeUpdateGrid(x - 1, y + 1, true);
-            } else {
-                ChangeGrid(x + 1, y + 1, 2);
-                ChangeUpdateGrid(x + 1, y + 1, true);
-            }
-        }
+		else if (ReturnGrid(x - 1, y + 1) == 0 && ReturnGrid(x + 1, y + 1) == 0){
+			ChangeGrid(x, y, 0);
+	
+			if (y % 2 == 0) {
+				ChangeGrid(x - 1, y + 1, 2);
+				ChangeUpdateGrid(x - 1, y + 1, true);
+			} else {
+				ChangeGrid(x + 1, y + 1, 2);
+				ChangeUpdateGrid(x + 1, y + 1, true);
+			}
+		}
 
-        else if (ReturnGrid(x - 1, y + 1) == 0) {
-            ChangeGrid(x, y, 0);
+		else if (ReturnGrid(x - 1, y + 1) == 0) {
+			ChangeGrid(x, y, 0);
 
-            if (ReturnGrid(x + 1, y + 1) == 1 || ReturnGrid(x + 1, y + 1) == 2 || ReturnGrid(x + 1, y + 1) == 3 || ReturnGrid(x + 1, y + 1) == 4) {
-                ChangeGrid(x - 1, y + 1, 2);
-                ChangeUpdateGrid(x - 1, y + 1, true);
-            }
-        }
+			if (ReturnGrid(x + 1, y + 1) == 1 || ReturnGrid(x + 1, y + 1) == 2 || ReturnGrid(x + 1, y + 1) == 3 || ReturnGrid(x + 1, y + 1) == 4) {
+				ChangeGrid(x - 1, y + 1, 2);
+				ChangeUpdateGrid(x - 1, y + 1, true);
+			}
+		}
 
-        else if (ReturnGrid(x + 1, y + 1) == 0) {
-            ChangeGrid(x, y, 0);
+		else if (ReturnGrid(x + 1, y + 1) == 0) {
+			ChangeGrid(x, y, 0);
 
-            if (ReturnGrid(x - 1, y + 1) == 1 || ReturnGrid(x - 1, y + 1) == 2 || ReturnGrid(x - 1, y + 1) == 3 || ReturnGrid(x - 1, y + 1) == 4) {
-                ChangeGrid(x + 1, y + 1, 2);
-                ChangeUpdateGrid(x + 1, y + 1, true);
-            }
-        }
+			if (ReturnGrid(x - 1, y + 1) == 1 || ReturnGrid(x - 1, y + 1) == 2 || ReturnGrid(x - 1, y + 1) == 3 || ReturnGrid(x - 1, y + 1) == 4) {
+				ChangeGrid(x + 1, y + 1, 2);
+				ChangeUpdateGrid(x + 1, y + 1, true);
+			}
+		}
 
-        else {
-            ChangeGrid(x, y, 2);
-        }
-    }
+		else {
+			ChangeGrid(x, y, 2);
+		}
+	}
 	else {
 		AddGravity(x, y, 2, 1);
 	}
 }
 
 void Cell::UpdateStone(int x, int y) {
-    if (y == height - 1 || x == width - 1 || x == 0) {
-        ChangeGrid(x, y, 3);
-    } 
-    else if (ReturnGrid(x, y + 1) == 1 || ReturnGrid(x, y + 1) == 3 || ReturnGrid(x, y + 1) == 4) {
-        ChangeGrid(x, y, 3);
-    }
-    else {
-        AddGravity(x, y, 3, 1);
-    }
+	if (y == height - 1 || x == width - 1 || x == 0) {
+		ChangeGrid(x, y, 3);
+	} 
+	else if (ReturnGrid(x, y + 1) == 1 || ReturnGrid(x, y + 1) == 3 || ReturnGrid(x, y + 1) == 4) {
+		ChangeGrid(x, y, 3);
+	}
+	else {
+		AddGravity(x, y, 3, 1);
+	}
 }
 
 void Cell::UpdateWall(int x, int y) {
@@ -222,7 +222,7 @@ void Cell::UpdateWall(int x, int y) {
 
 void Cell::draw(sf::RenderTarget& target, sf::RenderStates states) const
 {
-    states.transform *= getTransform();
-    states.texture = NULL;
-    target.draw(cellVertices, states);
+	states.transform *= getTransform();
+	states.texture = NULL;
+	target.draw(cellVertices, states);
 }
--- src/UI.cpp
+++ src/UI.cpp
@@ -17,28 +17,28 @@ void UI::InitButtons(int ButtonArrayWidth, int ButtonArrayHeight) {
 }
 
 void UI::DrawButtons(int xPos, int yPos, float btnWidth, float btnHeight, int offset) {
-    for (int x = 0; x < ButtonArrayWidth; ++x) {
-        for (int y = 0; y < ButtonArrayHeight; ++y) {
-		    sf::Vertex* quad = &UIelements[(x + y * ButtonArrayWidth) * 4];
+	for (int x = 0; x < ButtonArrayWidth; ++x) {
+		for (int y = 0; y < ButtonArrayHeight; ++y) {
+			sf::Vertex* quad = &UIelements[(x + y * ButtonArrayWidth) * 4];
 	
 			quad[0].position = sf::Vector2f((xPos + (x * offset)), yPos + (y * offset));
-		    quad[1].position = sf::Vector2f((xPos + (x * offset)) + btnWidth, yPos + (y * offset));
-		    quad[2].position = sf::Vector2f((xPos + (x * offset)) + btnWidth, yPos + (y * offset) + btnHeight);
-		    quad[3].position = sf::Vector2f((xPos + (x * offset)), yPos + (y * offset) + btnHeight);
+			quad[1].position = sf::Vector2f((xPos + (x * offset)) + btnWidth, yPos + (y * offset));
+			quad[2].position = sf::Vector2f((xPos + (x * offset)) + btnWidth, yPos + (y * offset) + btnHeight);
+			quad[3].position = sf::Vector2f((xPos + (x * offset)), yPos + (y * offset) + btnHeight);
 
-		    if (y == 0) {
-		    	// Eraser Color
-		    	quad[0].color = sf::Color(226, 174, 180);
-		    	quad[1].color = sf::Color(226, 174, 180);
-		    	quad[2].color = sf::Color(226, 174, 180);
+			if (y == 0) {
+				// Eraser Color
+				quad[0].color = sf::Color(226, 174, 180);
+				quad[1].color = sf::Color(226, 174, 180);
+				quad[2].color = sf::Color(226, 174, 180);
 				quad[3].color = sf::Color(226, 174, 180);
-		    } else {
-		    	quad[0].color = sf::Color(cellColors[y]);
-			    quad[1].color = sf::Color(cellColors[y]);
-			    quad[2].color = sf::Color(cellColors[y]);
+			} else {
+				quad[0].color = sf::Color(cellColors[y]);
+				quad[1].color = sf::Color(cellColors[y]);
+				quad[2].color = sf::Color(cellColors[y]);
 				quad[3].color = sf::Color(cellColors[y]);
-		    }
-        }
+			}
+		}
 	}
 }
 
@@ -58,7 +58,7 @@ bool UI::ButtonClicked(sf::Vector2i mousePosition) {
 }
 
 void UI::draw(sf::RenderTarget& target, sf::RenderStates states) const {
-    states.transform *= getTransform();
-    states.texture = NULL;
-    target.draw(UIelements, states);
+	states.transform *= getTransform();
+	states.texture = NULL;
+	target.draw(UIelements, states);
 }
